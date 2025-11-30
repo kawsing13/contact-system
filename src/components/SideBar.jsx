f309e76358d2cdcbc21951194dfbb08018bd7063
kawsing13 <jramos012097@gmail.com>
2025-12-01 07:07:06 +0800

    Update contact lists.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom"
 export default function Sidebar() {
   return (
     <div
-      className="bg-light text-dark position-fixed"
-      style={{ width: "250px", height: "100vh", top: "56px", left: 0 }}
+      className="bg-light position-fixed"
+      style={{ width: "250px", height: "100vh", top: "56px", left: 0,color: "black" }}
     >
       <ul className="nav flex-column p-3">
         <li className="nav-item mb-2">
-          <Link className="nav-link text-white" to="/dashboard">Dashboard</Link>
+          <Link className="nav-link text-black" to="/dashboard">Dashboard</Link>
         </li>
         <li className="nav-item mb-2">
-          <Link className="nav-link text-white" to="/users">Users</Link>
+          <Link className="nav-link text-black" to="/users">Users</Link>
         </li>
         <li className="nav-item mb-2">
-          <Link className="nav-link text-white" to="/settings">Settings</Link>
+          <Link className="nav-link text-black" to="/settings">Settings</Link>
         </li>
       </ul>
     </div>
